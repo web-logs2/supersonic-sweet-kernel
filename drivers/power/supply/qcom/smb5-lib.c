11d1e2f9a98a239fe3ddcdbdd1c3f57bb27e9f5f
Novik-XIV <glovesnovik@gmail.com>
2023-04-02 11:20:30 +0000

    power:fix no member named

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -3993,69 +3993,6 @@ static int smblib_set_sw_conn_therm_regulation(struct smb_charger *chg,
 	return rc;
 }
 
-static void smblib_reverse_boost_work(struct work_struct *work)
-{
-	struct smb_charger *chg = container_of(work, struct smb_charger, reverse_boost_work.work);
-	union power_supply_propval pval = {0, };
-	int rc = 0;
-	u8 reg = 0;
-
-	if (!chg->reverse_boost_wa)
-		return;
-
-	if (chg->real_charger_type == POWER_SUPPLY_TYPE_USB_CDP || chg->real_charger_type == POWER_SUPPLY_TYPE_USB)
-		return;
-	else if (chg->real_charger_type != POWER_SUPPLY_TYPE_USB_HVDCP_3 && chg->real_charger_type != POWER_SUPPLY_TYPE_USB_HVDCP_3P5 && chg->pd_active != POWER_SUPPLY_PD_PPS_ACTIVE)
-		goto clear_count;
-
-	rc = smblib_read(chg, AICL_STATUS_REG, &reg);
-	if (rc < 0 || !(reg & ICL_MIN_BIT))
-		goto clear_count;
-
-	rc = smblib_read(chg, TYPE_C_SENSOR_SM_STATUS_REG, &reg);
-	if (rc < 0 || (reg & VBUS_VSAFE5V_BIT))
-		goto clear_count;
-
-	rc = smblib_read(chg, INT_RT_STS_REG, &reg);
-	if (rc < 0 || !(reg & USBIN_PLUGIN_RT_STS_BIT))
-		goto clear_count;
-
-	if (!chg->cp_psy)
-		chg->cp_psy = power_supply_get_by_name("bq2597x-standalone");
-
-	if (!chg->bms_psy || !chg->cp_psy || !chg->usb_psy)
-		goto clear_count;
-
-	rc = power_supply_get_property(chg->cp_psy, POWER_SUPPLY_PROP_CHARGING_ENABLED, &pval);
-	if (pval.intval)
-		goto clear_count;
-
-	rc = power_supply_get_property(chg->bms_psy, POWER_SUPPLY_PROP_CURRENT_NOW, &pval);
-	if ((pval.intval / 1000) <= REVERSE_BOOST_MIN_IBAT_MA)
-		goto clear_count;
-
-	rc = power_supply_get_property(chg->usb_psy, POWER_SUPPLY_PROP_INPUT_CURRENT_NOW, &pval);
-	if ((pval.intval / 1000) >= REVERSE_BOOST_MAX_IBUS_MA)
-		goto clear_count;
-
-	chg->reverse_count++;
-	goto recheck;
-
-clear_count:
-	chg->reverse_count = 0;
-
-recheck:
-	if (chg->reverse_count >= 2) {
-		smblib_masked_write(chg, USBIN_CMD_IL_REG, USBIN_SUSPEND_BIT, USBIN_SUSPEND_BIT);
-		msleep(250);
-		smblib_masked_write(chg, USBIN_CMD_IL_REG, USBIN_SUSPEND_BIT, 0);
-	} else {
-		schedule_delayed_work(&chg->reverse_boost_work, msecs_to_jiffies(REVERSE_BOOST_WORK_RECHECK_DELAY_MS));
-	}
-
-	return;
-}
-
 #define CHG_DETECT_CONN_THERM_US		120000000	/* 120sec */
 static void smblib_conn_therm_work(struct work_struct *work)
 {
@@ -6807,8 +6744,6 @@ void smblib_usb_plugin_hard_reset_locked(struct smb_charger *chg)
 	vbus_rising = (bool)(stat & USBIN_PLUGIN_RT_STS_BIT);
 
 	if (vbus_rising) {
-		chg->reverse_count = 0;
-		schedule_delayed_work(&chg->reverse_boost_work, msecs_to_jiffies(REVERSE_BOOST_WORK_START_DELAY_MS));
 		/* hold chg_awake wakeup source when charger is present */
 		vote(chg->awake_votable, CHG_AWAKE_VOTER, true, 0);
 		/* Remove FCC_STEPPER 1.5A init vote to allow FCC ramp up */
@@ -6830,8 +6765,6 @@ void smblib_usb_plugin_hard_reset_locked(struct smb_charger *chg)
 			}
 		}
 
-		chg->reverse_count = 0;
-		cancel_delayed_work_sync(&chg->reverse_boost_work);
 		cancel_delayed_work_sync(&chg->charger_type_recheck);
 		chg->hvdcp_recheck_status = false;
 		chg->recheck_charger = false;
